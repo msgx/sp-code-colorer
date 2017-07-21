723805878295ecb859fa70e625e1fa2c1fc6c5d2
Alisher Abdurakhmanov <msgx@users.noreply.github.com>
2017-07-21 23:26:49 +0300

    Update packages; configure minification of bundle.js; remove sourcemap

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,12 @@ module.exports = {
 		filename: "bundle.js",
 		path: __dirname + "/scripts"
 	},
-	devtool: "source-map",
 	resolve: {
 		extensions: [".ts", ".tsx", ".js", ".jsx", ".json"]
 	},
 	module: {
 		rules: [
-			{ test: /\.tsx?$/, loader: "ts-loader" },
-			{ test: /\.js$/, loader: "source-map-loader", enforce: "pre" }
+			{ test: /\.tsx?$/, loader: "ts-loader" }
 		]
 	},
 	externals: {
